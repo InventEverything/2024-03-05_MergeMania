87d24b4a066d8a6bb3ee273f2fa4dc73e6c5b05b
Ian Engle <ian.engle@student.centralia.edu>
2024-03-05 09:47:46 -0800

    timer counts to a variable

--- 2024-03-05_MergeMania/Form1.cs
+++ 2024-03-05_MergeMania/Form1.cs
@@ -13,6 +13,7 @@ namespace _2024_03_05_MergeMania
     public partial class Form1 : Form
     {
         int i;
+        int j = 500;
         public Form1()
         {
             InitializeComponent();
@@ -21,15 +22,16 @@ namespace _2024_03_05_MergeMania
         private void button1_Click(object sender, EventArgs e)
         {
             i = 0;
+            j = j / 2;
             timer1.Enabled = true;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             i += 1;
-            label1.Text = i.ToString();
-            label1.ForeColor = Color.FromArgb((int)(i*.255), (int)(i *.055), (int)(i *.255));
-            if (i >= 1000)
+            label1.Text = i.ToString() + " Hello!";
+            label1.ForeColor = Color.FromArgb((int)(i*50/j), (int)(i * 255/j), (int)(i * 50/j));
+            if (i >= j)
                 timer1.Enabled = false;
         }
     }
